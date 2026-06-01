1c225120b6e8a7e469445cd66440d21b4b6d4e6e
vidhya v <43576737+vidhya1s@users.noreply.github.com>
2026-06-01 12:57:30 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -25,7 +25,7 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
+    # Set the input values you want to test.
     dut.ui_in.value = 20
     dut.uio_in.value = 30
 
